7c05d48d450fd37e9f8a6c7adcb21e5a169ff874
Mach Kobayashi <machk@apple.com>
2023-04-30 13:21:10 -0700

    Textured cycle, played with lighting effects

--- Assets/Shaders/ShaderFunctions.hlsl
+++ Assets/Shaders/ShaderFunctions.hlsl
@@ -3,10 +3,10 @@
 
 void Grid_float(float2 UV, float2 Ddxy, out float3 _out) {
     float gridX = abs((_Grid_Size * UV.x) % 1 - 0.5);
-    gridX = smoothstep(0.5 - 5 * _Grid_Size * Ddxy.x, 0.5, gridX);
+    gridX = smoothstep(0.5 - 2 * _Grid_Size * Ddxy.x, 0.5, gridX);
     float gridY = abs((_Grid_Size * UV.y) % 1 - 0.5);
-    gridY = smoothstep(0.5 - 5 * _Grid_Size * Ddxy.y, 0.5, gridY);
-    float grid = max(gridX, gridY) / (1 + 5 * (_Grid_Size * Ddxy.x + _Grid_Size * Ddxy.y));
+    gridY = smoothstep(0.5 - 2 * _Grid_Size * Ddxy.y, 0.5, gridY);
+    float grid = max(gridX, gridY) / (1 + 10 * (_Grid_Size * Ddxy.x + _Grid_Size * Ddxy.y));
     _out = _Intensity * _Grid_Color * grid;
 }
 
